52be63b1086f52d94f846c6a795b124633687049
WEI YING ENG <weiyingeng@WEIs-MacBook-Pro.local>
2016-10-09 19:10:23 +0800

    added email template

--- app/mailers/application_notification_mailer.rb
+++ app/mailers/application_notification_mailer.rb
@@ -1,2 +1,9 @@
 class ApplicationNotificationMailer < ApplicationMailer
+
+  def notification_email(user)
+    @user = user
+    @url  = '/listings/#{@listing.id}'
+    mail(to: @user.email, subject: 'Application for Job Listing')
+  end
+
 end
