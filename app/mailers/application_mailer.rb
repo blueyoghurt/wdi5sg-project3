bf5ce96c62970441535ac3fff624aeb60ad56ad2
WEI YING ENG <weiyingeng@WEIs-MacBook-Pro.local>
2016-10-10 13:50:37 +0800

    minor bugs

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'jobslitesg@gmail.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,9 @@ Rails.application.routes.draw do
 
   resources :listings
   resources :applications
-  resources :bizowners  
+  resources :bizowners
+  resources :jobseekers
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
