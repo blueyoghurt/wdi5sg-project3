c06c8b984c4474dd20c85a5947b6c69b58d5d9c4
dexterleow <gdleow@gmail.com>
2016-10-10 10:42:41 +0800

    Change some columns in database

--- config/routes.rb
+++ config/routes.rb
@@ -34,5 +34,7 @@ Rails.application.routes.draw do
   put "business/profile" => "bizowners#update"
   patch "business/profile" => "bizowners#update"
 
+  resources :bizowners
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
