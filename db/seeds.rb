99e9d04ff305f905b30f6e733f0852cdce93a337
Luke <blue.yoghurt@gmail.com>
2016-10-12 10:18:10 +0800

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,50 @@ User.find_or_create_by!(email: ENV["seed_email"]) do |user|
   user.first_name = 'dexter'
   user.last_name = 'wei ying'
   user.is_seeker = false
+  user.is_biz = false
+  user.is_admin = false
+  user.password = ENV["seed_password"]
+  user.contact = 12345678
+end
+
+User.find_or_create_by!(email: ENV["biz_email"]) do |user|
+  user.first_name = 'Bizowner'
+  user.last_name = 'One'
+  user.is_seeker = false
   user.is_biz = true
   user.is_admin = true
   user.password = ENV["seed_password"]
   user.contact = 12345678
 end
 
-arealocations = Arealocation.create([{id: 1, area: "North", location:"Ang Mo Kio"},{id: 2, area: "North", location: "Bishan"}])
+User.find_or_create_by!(email: ENV["job_email"]) do |user|
+  user.first_name = 'JobSeeker'
+  user.last_name = 'One'
+  user.is_seeker = true
+  user.is_biz = false
+  user.is_admin = true
+  user.password = ENV["seed_password"]
+  user.contact = 12345678
+end
+
+Bizowner.find_or_create_by!(user_id: User.find_by(email: ENV["biz_email"]).id) do |biz|
+  biz.user_id = User.find_by(email: ENV["biz_email"]).id
+  biz.name = 'ABC Limited'
+  biz.address = '123 Main Street'
+  biz.postal_code = "643282"
+  biz.description = 'Sells ABC beer'
+  biz.license_number = 'JSFA324789'
+end
+
+Jobseeker.find_or_create_by!(user_id: User.find_by(email: ENV["job_email"]).id) do |job|
+  job.user_id = User.find_by(email: ENV["job_email"]).id
+  job.dob = '1990-10-31'
+  job.postal_code = "643282"
+  job.highest_qualification = 'Sells ABC beer'
+  job.preferred_area = 'North'
+  job.preferred_location = 'Ang Mo Kio'
+  job.description = 'Hardworker'
+  job.wage = 7
+  job.start_date = '2016-10-01'
+  job_end_date = '2016-10-31'
+end
