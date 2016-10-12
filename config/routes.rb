4837b0af24bd3a9d25e3f7a9fd246ed92ec73663
dexterleow <gdleow@gmail.com>
2016-10-13 02:39:39 +0800

    CSS changes in Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rails.application.routes.draw do
   resources :bizowners
   resources :stores
   resources :jobs
-# root 'jobs#index'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
